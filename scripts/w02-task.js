1cff552db342075d21d2b8a97176a7901728cc09
joao <jmoita@fusionware.com>
2024-01-20 12:52:52 -0700

    finished week 2 assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,45 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+const fullName = 'Joao Moita';
 
+let currentYear = '2023';
 
-
+let profilePicture = 'images/picture.jpg';
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
 
+const foodElement = document.getElementById('food');
 
+const yearElement = document.querySelector('#year');
+
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
+yearElement.textContent = currentYear;
 
+imageElement.setAttribute('src', profilePicture);
 
-
+imageElement.setAttribute('alt', `image of ${fullName}`);
 
 /* Step 5 - Array */
 
+const favoriteFood = ['Pizza', 'Spaghetti', 'Mexican', 'Sushi', 'Chinese'];
 
+foodElement.innerHTML = favoriteFood;
 
+const foodItem = 'Japanese'
 
+favoriteFood.push(foodItem);
 
+foodElement.innerHTML += `<br>${favoriteFood}`;
 
+favoriteFood.shift();
+
+foodElement.innerHTML += `<br>${favoriteFood}`;
