0d72916246f36d89bf3f1242f848297df681b498
joao <jmoita@fusionware.com>
2024-02-12 23:17:50 -0700

    added all filter

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,71 +2,86 @@
 
 /* Declare and initialize global variables */
 
-document.querySelector('#filtered').addEventListener("change", () => { filterTemples(templeList)})
+document.querySelector("#filtered").addEventListener("change", () => {
+  filterTemples(templeList);
+});
 
-const templesElement = document.querySelector('#temples')
+const templesElement = document.querySelector("#temples");
 
-let templeList = []
-
+let templeList = [];
 
 /* async displayTemples Function */
 
 const displayTemples = (temples) => {
-    for (i = 0; i < temples.length; i++){
-        const article = document.createElement("article");
-        const templeName = document.createElement("h3");
-        const templeNameText = document.createTextNode(templeList[i].templeName)
-        const templeImage = document.createElement("img");
-        templeImage.src = templeList[i].imageUrl;
-        templeName.appendChild(templeNameText)
-        article.appendChild(templeName);
-        article.appendChild(templeImage);
-        templesElement.appendChild(article);
-    }
-}
+  for (i = 0; i < temples.length; i++) {
+    const article = document.createElement("article");
+    const templeName = document.createElement("h3");
+    const templeNameText = document.createTextNode(templeList[i].templeName);
+    const templeImage = document.createElement("img");
+    templeImage.src = templeList[i].imageUrl;
+    templeName.appendChild(templeNameText);
+    article.appendChild(templeName);
+    article.appendChild(templeImage);
+    templesElement.appendChild(article);
+  }
+};
 
 /* async getTemples Function using fetch()*/
 
 const getTemples = async () => {
-    const response = await fetch ('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
-      if (response.ok) {
-        const data = await response.json();
-        getData(data);
-      }
-    function getData(data) {
-      templeList = data;
-      console.log(templeList)
-      displayTemples(templeList)
-    }
-}
+  const response = await fetch(
+    "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
+  );
+  if (response.ok) {
+    const data = await response.json();
+    getData(data);
+  }
+  function getData(data) {
+    templeList = data;
+    console.log(templeList);
+    displayTemples(templeList);
+  }
+};
 
 /* reset Function */
 
 const reset = () => {
-    templeList = []
-    templesElement.innerHTML = '';
-}
+  templesElement.innerHTML = "";
+};
 /* filterTemples Function */
 
-const filterTemples = (temples) => {
-    reset();
-    let filter = document.getElementById('filtered').value;
-    switch (filter) {
-      case 'utah':  console.log('test data 1');
-        break;
-      case 'nonutah': console.log('test data 2');
-        break;
-      case 'older': console.log('test data 3');
-        break;
-      case 'all': console.log('test data 4');
-      default: return true;
+const filterTemples = async (temples) => {
+  reset();
+  let filter = document.getElementById("filtered").value;
+  switch (filter) {
+    case "utah":
+      for (let i = 0; i < temples.length; i++){
+        displayTemples(temples[i])
+        console.log(temples[i])
+      }
 
+
+
+      break;
+
+    case "nonutah":
+      console.log("test data 2");
+      break;
+    case "older":
+      console.log("test data 3");
+      break;
+    case "all":
+      console.log("test data 4");
+      displayTemples(temples)
+    default:
+      return true;
   }
-}
+};
 
 getTemples();
 
 /* Event Listener */
 
-
-document.querySelector('#filtered').addEventListener('change', () => {filterTemples(templeList) });
+document.querySelector("#filtered").addEventListener("change", () => {
+  filterTemples(templeList);
+});
